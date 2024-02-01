5a9344e186827bb02c1d27b390db975e0069cb50
Code Delegates <CodeDelegates@gmail.com>
2024-02-01 16:02:14 +0100

    Resender

--- index.js
+++ index.js
@@ -11,23 +11,19 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
-
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignore errors.
-      //
-    });
-  }, 100);
-  console.log('started client interval');
-
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
-  });
+wss.on('connection', (ws) => {
+	ws.on('message', (data) => {
+		console.log('data received \n ' + data);
+		wss.clients.forEach(function (client) {
+			client.send(data.toString());
+		});
+	});
+	ws.on('close', function () {
+		//console.log('stopping client interval');
+		//clearInterval(id);
+	});
 });
 
-server.listen(8080, function () {
-  console.log('Listening on http://0.0.0.0:8080');
+server.listen(4000, function () {
+	console.log('Listening on http://0.0.0.0:4000');
 });
